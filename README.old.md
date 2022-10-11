97840a41be900cca845f1eddef56866504f27354
Steven Dorvil <dorvilsteven@gmail.com>
2022-10-11 06:43:00 -0400

    inital commit, create react app, page title setup

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# nba-standings-app
+nba standing app built with react and a drag and drop library
---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import { DragDropContext } from 'react-beautiful-dnd';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h2>NBA Standings Maker</h2>
+        <h6>Drag and Drop NBA teams in order to Rank them.</h6>
+        {}
+      </header>
+    </div>
+
+  );
+}
+
+export default App;
